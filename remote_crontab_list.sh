1714d81b5ac482505fbdbdb8ff68884d8b235ab7
nick814 <42017124+nick814@users.noreply.github.com>
2021-04-25 10:12:21 +0800

    Update remote_crontab_list.sh

--- remote_crontab_list.sh
+++ remote_crontab_list.sh
@@ -9,3 +9,25 @@
 15 15 * * * node /scripts/monk_shop_follow_sku.js >> /scripts/logs/monk_shop_follow_sku.log 2>&1
 # 店铺大转盘
 3 0,10,23 * * * node /scripts/monk_shop_lottery.js >> /scripts/logs/monk_shop_lottery.log 2>&1
+# 直播间抽奖
+5 8-23/1 * * * node /scripts/jd_live_lottery_social.js >> /scripts/logs/jd_live_lottery_social.log 2>&1
+# 整点京豆雨
+1 0-23 * * * node /scripts/lxk0301_live_redrain.js >> /scripts/logs/lxk0301_live_redrain.log 2>&1
+# 加购有礼
+15 12 * * * node /scripts/monk_shop_add_to_car.js >> /scripts/logs/monk_shop_add_to_car.log 2>&1
+# 有机牧场
+0 0,1-22/2 1-31 4-7 * node /scripts/monk_pasture.js >> /scripts/logs/monk_pasture.log 2>&1
+# 京东小魔方
+10 10 7-9 4 * node /scripts/z_xmf.js >> /scripts/logs/z_xmf.log 2>&1
+# 粉丝互动
+3 10 * * * node /scripts/z_fanslove.js >> /scripts/logs/z_fanslove.log 2>&1
+# 母婴-跳一跳
+5 8,14,20 6-13 4 * node /scripts/z_mother_jump.js >> /scripts/logs/z_mother_jump.log 2>&1
+# 摇一摇
+3 20 * * * node /scripts/z_shake.js >> /scripts/logs/z_shake.log 2>&1
+# 京东小窝
+0 9 * * * node /scripts/ddxw.js >> /scripts/logs/ddxw.log 2>&1
+# 半自动领京豆
+30 18 * * * node /scripts/jd_zjd.js >> /scripts/logs/jd_zjd.log 2>&1
+# 京东赚京东开团
+10 * * * * node /scripts/jd_zjd_tuan.js >> /scripts/logs/jd_zjd_tuan.log 2>&1
